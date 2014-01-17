32de6d9e3413901073123e96fc71f2d05372a751
dmartz86 <dmartz86@gmail.com>
2014-01-17 15:41:26 -0500

    Updating the tests

--- test-test.js
+++ test-test.js
@@ -1,24 +1,24 @@
 /**
  * Populate
  */
-TestJS.LIST.push(TestJS.create(false,false,false));
-TestJS.LIST.push(TestJS.create(false,false,false,true));
-TestJS.LIST.push(TestJS.create(false,false,''));
-TestJS.LIST.push(TestJS.create(false,false,'',true));
-TestJS.LIST.push(TestJS.create(false,false,'Boolean test'));
-TestJS.LIST.push(TestJS.create(false,false,'Second boolean test',true));
-TestJS.LIST.push(TestJS.create(false,true,'Third boolean test'));
-TestJS.LIST.push(TestJS.create(true,true,'Four boolean test',true));
-TestJS.LIST.push(TestJS.create("text",true,'String test'));
-TestJS.LIST.push(TestJS.create("text",true,'Second String test'),true);
-TestJS.LIST.push(TestJS.create(1,true,'Number agains boolean'));
-TestJS.LIST.push(TestJS.create(1,true,'Number 2 agains boolean'));
-TestJS.LIST.push(TestJS.create(1.093,true,'Decimal agains boolean'));
-TestJS.LIST.push(TestJS.create(1.093,true,'Decimal 2 agains boolean'));
-TestJS.LIST.push(TestJS.create({test:''},{test:''},'Object test'));
-TestJS.LIST.push(TestJS.create({test:''},{test:''},'Object strict test'),true);
-TestJS.LIST.push(TestJS.create({test:''},{test:''},'Testing class css'));
-TestJS.LIST.push(TestJS.create(false,'','Testing class css strict',true));
+TestJS.create(false,false,false);
+TestJS.create(false,false,false,true);
+TestJS.create(false,false,'');
+TestJS.create(false,false,'',true);
+TestJS.create(false,false,'Boolean test');
+TestJS.create(false,false,'Second boolean test',true);
+TestJS.create(false,true,'Third boolean test');
+TestJS.create(true,true,'Four boolean test',true);
+TestJS.create("text",true,'String test');
+TestJS.create("text",true,'Second String test',true);
+TestJS.create(1,true,'Number agains boolean');
+TestJS.create(1,true,'Number 2 agains boolean');
+TestJS.create(1.093,true,'Decimal agains boolean');
+TestJS.create(1.093,true,'Decimal 2 agains boolean');
+TestJS.create({test:''},{test:''},'Object test');
+TestJS.create({test:''},{test:''},'Object strict test',true);
+TestJS.create({test:''},{test:''},'Testing class css');
+TestJS.create(false,'','Testing class css strict',true);
 
 //Run method
 TestJS.run();
